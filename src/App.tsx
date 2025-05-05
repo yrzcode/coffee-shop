3c0abd320faa2d44dd5afdbcc158ecb4068bc371
rz_yang <yrzcode@hotmail.com>
2025-05-06 05:56:41 +0900

    add typescript to the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import SubTitle from "./components/SubTitle";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SubTitle />
+        <h1>Trackカフェ</h1>
+        <p>オライン注文システム</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SubTitle.tsx
@@ -0,0 +1,11 @@
+import styles from "../sass/subTitle.module.scss";
+
+const SubTitle = () => {
+  return (
+    <div className={styles.subtitle}>
+      ✨ 美味しいコーヒーを、いつでもどこでも ✨
+    </div>
+  );
+};
+
+export default SubTitle;
