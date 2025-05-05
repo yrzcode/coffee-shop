abf2e02c6b0042607a0ffce47a69b099b6e5e11b
rz_yang <yrzcode@hotmail.com>
2025-05-06 07:44:22 +0900

    remove react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,4 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
