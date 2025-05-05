0c65db3248d749fbb6c6f47ce471e1915b246f6e
rz_yang <yrzcode@hotmail.com>
2025-05-06 05:30:17 +0900

    add sass package, define global sass variables and subTitle for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import SubTitle from "./components/SubTitle";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
+				<SubTitle />
 				<h1>Trackカフェ</h1>
 				<p>オライン注文システム</p>
 			</header>
---
+++ src/components/SubTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../sass/subTitle.module.scss";
+
+export default function SubTitle() {
+	return (
+		<div className={styles.subtitle}>
+			✨ 美味しいコーヒーを、いつでもどこでも ✨
+		</div>
+	);
+}
